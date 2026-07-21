96d28dd323a4a05e5099df7c684e76d80c8d085a
Cristian Teijeiro <cristian.teijeiro@fusuma.io>
2026-07-21 13:07:23 +0200

    chore(db): add Drizzle migration metadata

--- drizzle/migrations/migrations.js
+++ drizzle/migrations/migrations.js
@@ -11,6 +11,7 @@ import m0006 from './0006_next_sasquatch.sql';
 import m0007 from './0007_regular_sheva_callister.sql';
 import m0008 from './0008_smart_gauntlet.sql';
 import m0009 from './0009_graceful_gideon.sql';
+import m0010 from './0010_modern_bishop.sql';
 
   export default {
     journal,
@@ -24,7 +25,8 @@ m0005,
 m0006,
 m0007,
 m0008,
-m0009
+m0009,
+m0010
     }
   }
   
